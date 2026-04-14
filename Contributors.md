85b05e96bca23415280217a04e7f9b1e80f41266
Nabeel Farooq <nabeelfarooq1515@gmail.com>
2026-04-14 16:51:54 +0500

    Initial commit

---
+++ Contributors.md
@@ -0,0 +1,44 @@
+# Contributors
+This file contains the list of everyone who contributed to the repository
+<br>
+<table>
+<th>Contributors</th><th>Contributions</th>  
+  <tr>
+    <td><img src="https://avatars.githubusercontent.com/blackmarketer?s=100">
+    <br>
+    <a href="https://github.com/blackmarketer">Alan Abhilash</a></td>
+    <td><a href="https://github.com/Anon-Artist/R3C0Nizer/pull/1">Contributions</a> by Alan Abhilash</td>
+  </tr>
+  <tr>
+    <td><img src="https://avatars.githubusercontent.com/E-R-R-O-R-404?s=100">
+    <br>
+    <a href="https://github.com/E-R-R-O-R-404">Vimal V</a></td>
+    <td><a href="https://github.com/Anon-Artist/R3C0Nizer/pull/2">Contributions</a> by Vimal V</td>
+  </tr>
+  <tr>
+    <td><img src="https://avatars.githubusercontent.com/Conscript-Security?s=100">
+    <br>
+    <a href="https://github.com/Conscript-Security">Jagan</a></td>
+    <td><a href="https://github.com/Anon-Artist/R3C0Nizer/pull/4">Contributions</a> by Jagan</td>
+  </tr>
+  <tr>
+    <td><img src="https://avatars.githubusercontent.com/v1nc1d4?s=100">
+    <br>
+    <a href="https://github.com/v1nc1d4">Anurag M</a></td>
+    <td><a href="https://github.com/Anon-Artist/R3C0Nizer/pull/5">Contributions</a> by Anurag M</td>
+  </tr>
+  <tr>
+    <td><img src="https://avatars.githubusercontent.com/Shahul-Aboobaker?s=100">
+    <br>
+    <a href="https://github.com/Shahul-Aboobaker">Shahul Aboobaker</a></td>
+    <td><a href="https://github.com/Anon-Artist/R3C0Nizer/pull/11">Contributions</a> by Shahul Aboobaker</td>
+  </tr>
+  <tr>
+    <td><img src="https://avatars.githubusercontent.com/GovindPalakkal?s=100">
+    <br>
+    <a href="https://github.com/GovindPalakkal">Govind Palakkal</a></td>
+    <td><a href="https://github.com/Anon-Artist/R3C0Nizer/blob/main/src/blcscan.sh">Contributions</a> by Govind Palakkal</td>
+  </tr>
+</table>
+<br>
+### Thanks to everyone who helped in building this Repository :)
---
+++ src/visual_recon.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+#colors
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+reset=`tput sgr0`
+
+read -p "Enter domain name : " DOM
+
+if [ -d ~/reconizer/ ]
+then
+  echo " "
+else
+  mkdir ~/reconizer
+fi
+
+if [ -d ~/reconizer/$DOM ]
+then
+  echo " "
+else
+  mkdir ~/reconizer/$DOM
+
+fi
+
+if [ -d ~/reconizer/$DOM/Visual_Recon ]
+then
+  echo " "
+else
+  mkdir ~/reconizer/$DOM/Visual_Recon
+
+fi
+
+
+echo "${red}
+ =================================================
+|   ____  _____  ____ ___  _   _ _                |
+|  |  _ \|___ / / ___/ _ \| \ | (_)_______ _ __   |
+|  | |_) | |_ \| |  | | | |  \| | |_  / _ \ '__|  |
+|  |  _ < ___) | |__| |_| | |\  | |/ /  __/ |     |
+|  |_| \_\____/ \____\___/|_| \_|_/___\___|_|     |
+|                                                 |
+ ================== Anon-Artist ==================
+${reset}"
+echo "${blue} [+] Starting Visual Recon ${reset}"
+echo " "
+
+#screenshotting
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+echo " "
+if [ -f ~/go/bin/aquatone ]
+then
+  echo "${magenta} [+] Running Aquatone for screenshotting alive subdomains${reset}"
+  cat ~/reconizer/$DOM/Subdomains/unique.txt | aquatone -http-timeout 10000 -scan-timeout 300 -ports xlarge -out ~/reconizer/$DOM/Visual_Recon
+else
+  echo "${blue} [+] Installing Aquatone ${reset}"
+  go get github.com/michenriksen/aquatone
+  echo "${magenta} [+] Running Aquatone for screenshotting alive subdomains${reset}"
+  cat ~/reconizer/$DOM/Subdomains/unique.txt | aquatone -http-timeout 10000 -scan-timeout 300 -ports xlarge -out ~/reconizer/$DOM/Visual_Recon
+fi
+
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+echo ""
+echo "${blue} [+] Successfully saved the results"
+echo ""
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+echo ""
+echo "${red} [+] Thank you for using R3C0Nizer${reset}"
+echo ""
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
